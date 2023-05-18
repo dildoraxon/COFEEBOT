11fc251a904fe9f9fda29dc1d223aeebd8310fb3
dildoraxon <mahkamovamadinaxon286@gmail.com>
2023-05-18 15:41:38 +0500

    first commit

---
+++ bot.py
@@ -0,0 +1,20 @@
+
+from aiogram import Bot,Dispatcher,types,executor
+import logging
+from api import get_coffee
+logging.basicConfig(level=logging.INFO)
+
+bot=Bot(token="5890052860:AAESeAa7DqFbJalKdH7jIwg1AO5AHIOLs9A")
+dp=Dispatcher(bot)
+
+@dp.message_handler(commands="start")
+async def salomlash(message:types.Message):
+    await message.answer("Salom men senga coffe yuboraman /cofe ni bos")
+
+@dp.message_handler(commands="cofe")
+async def cofe(message:types.Message):
+    rasm=get_coffee()
+    await message.answer(rasm)
+
+if __name__=="__main__":
+    executor.start_polling(dp,skip_updates=True)
